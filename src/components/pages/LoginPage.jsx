4c7278c0240776c9cab6fd342894a5b06755b199
VL <lusakvm@gmail.com>
2023-10-17 22:04:57 +0300

    login page

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div  className='mt-4'>
+      <h1 className='text-4xl text-center'>Login</h1>
+      <form className='max-w-md mx-auto'>
+        <input type='email' placeholder='your@email.com'/>
+        <input type='password' placeholder='password'/>
+        <button>Login</button>      
+      </form>
+    </div>
   )
 }
