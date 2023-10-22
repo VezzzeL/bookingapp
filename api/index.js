5624a8ca987bcbf1eeb322ce14db0b9838dfc3be
VL <lusakvm@gmail.com>
2023-10-22 18:01:33 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (req, res) => {
-    res.json('test ok')
+  res.json("test ok");
 });
 
-
-app.listen(4000)
+app.listen(4000);
