3bd7fd8dd114ba378a8d9e299584eb0c8a5cfecb
VL <lusakvm@gmail.com>
2023-10-26 01:35:09 +0300

    login complete

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "6539844e526e3bc20e0e0e2f";
 
 app.use(express.json());
 
@@ -34,6 +36,29 @@ app.post("/register", async (req, res) => {
     res.json({ userDoc });
   } catch (e) {
     res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json("pass ok");
+        }
+      );
+    } else {
+      res.status(422).json("pass wrong");
+    }
+  } else {
+    res.json("not found");
   }
 });
 
--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post("/login", { email, password });
+      alert("Login successful");
+      setRedirect(true);
+    } catch {
+      alert("Login failed");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-128">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="login primary">Login</button>
           <div className="text-center py-2 text-gray-500">
             Don't have an account yet?{" "}
