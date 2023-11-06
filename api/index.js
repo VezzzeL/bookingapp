1ab6e60edfa4f1cd7d0af5c4a676e77cc30e7e96
VL <lusakvm@gmail.com>
2023-11-06 22:10:37 +0200

    post places

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
+const Place = require("./models/Place.js");
 const cookieParser = require("cookie-parser");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
@@ -110,6 +111,37 @@ app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
     uploadFiles.push(newPath.replace("uploads", ""));
   }
   res.json(uploadFiles);
+});
+
+app.post("/places", (req, res) => {
+  const { token } = req.cookies;
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuest,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id,
+      title,
+      address,
+      addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuest,
+    });
+    res.json(placeDoc);
+  });
 });
 
 app.listen(4000);
