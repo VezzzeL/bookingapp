13d1488df40a58a1ff69380979d3bce11a4e2bd6
VL <lusakvm@gmail.com>
2023-11-13 21:51:33 +0200

    index page update

--- client/src/components/pages/IndexPage.jsx
+++ client/src/components/pages/IndexPage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const IndexPage = () => {
-  <div>index page</div>;
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get("/places").then((response) => {
+      //TODO: remove later after adding more places
+      setPlaces([...response.data, ...response.data]);
+    });
+  }, []);
+  return (
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {places.length > 0 &&
+        places.map((place) => (
+          <Link to={"/place/" + place._id}>
+            <div className="bg-gray-500 mb-2 rounded-2xl flex">
+              {place.photos?.[0] && (
+                <img
+                  className="rounded-2xl object-cover aspect-square"
+                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                  alt=""
+                />
+              )}
+            </div>
+            <h2 className="font-bold">{place.title}</h2>
+            <h3 className="text-sm text-gray-500">{place.address}</h3>
+            <div className="mt-1">
+              ₴ <span className="font-bold">{place.price}</span>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
 };
