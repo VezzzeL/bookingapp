73dc28c8de16a7fb0b5ca3aabfcaf62ead0fcd5f
VL <lusakvm@gmail.com>
2023-11-15 22:59:11 +0200

    placePage update

--- client/src/components/pages/PlacePage.jsx
+++ client/src/components/pages/PlacePage.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 export const PlacePage = () => {
   const { id } = useParams();
   const [place, setPlace] = useState(null);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
   useEffect(() => {
     if (!id) {
       return;
@@ -15,12 +16,105 @@ export const PlacePage = () => {
   }, [id]);
 
   if (!place) return "";
+
+  if (showAllPhotos) {
+    return (
+      <div className="absolute inset-0 bg-white min-h-screen">
+        <div className="p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <button className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl sharow shadow-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              Close photos
+            </button>
+            {place?.photos?.length > 0 &&
+              place.photos.map((photo) => (
+                <div>
+                  <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
-      <h1 className="text-2xl">{place.title}</h1>
-      <a target="_blank" href={"httpss://maps.google.com/?g=" + place.address}>
+      <h1 className="text-3xl">{place.title}</h1>
+      <a
+        className="my-2 block font-semibold underline"
+        href={"https://maps.google.com/?g=" + place.address}
+      >
         {place.address}
       </a>
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+          <div>
+            {place.photos?.[0] && (
+              <div>
+                <img
+                  className="aspect-square object-cover min-w-full min-h-full"
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
+          <div className="grid">
+            {place.photos?.[1] && (
+              <img
+                className="aspect-square object-cover min-w-full min-h-full"
+                src={"http://localhost:4000/uploads/" + place.photos[1]}
+                alt=""
+              />
+            )}
+            <div className="overflow-hidden">
+              {place.photos?.[2] && (
+                <img
+                  className="aspect-square object-cover min-w-full min-h-full relative top-2"
+                  src={"http://localhost:4000/uploads/" + place.photos[2]}
+                  alt=""
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            setShowAllPhotos(true);
+          }}
+          className="flex gap-1 absolute bottom-1 right-1 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+          Show more photos
+        </button>
+      </div>
     </div>
   );
 };
