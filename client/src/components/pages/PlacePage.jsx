16dc3cc4afdfd35e0d27c2ae5f507278dd6bf3c6
VL <lusakvm@gmail.com>
2023-11-26 23:25:45 +0200

    Added BookingWidget + completed PlacePage

--- client/src/components/pages/PlacePage.jsx
+++ client/src/components/pages/PlacePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { BookingWidget } from "../BookingWidget";
 
 export const PlacePage = () => {
   const { id } = useParams();
@@ -58,7 +59,7 @@ export const PlacePage = () => {
   }
 
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         className="flex gap-1 my-3 font-semibold underline"
@@ -141,17 +142,27 @@ export const PlacePage = () => {
           Show more photos
         </button>
       </div>
-      <div className="my-4">
-        <h2 className="font-semibold text-2xl">Description</h2>
-        {place.description}
-      </div>
-      <div className="grid grid-cols-2">
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
           Check-in: {place.checkIn} <br />
           Check-out: {place.checkOut} <br />
           Max number of guests: {place.maxGuests}
         </div>
-        <div></div>
+        <div>
+          <BookingWidget place={place} />
+        </div>
+      </div>
+      <div className="bg-white -mt-8 px-8 py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info:</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">
+          {place.extraInfo}
+        </div>
       </div>
     </div>
   );
