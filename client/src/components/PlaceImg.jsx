29454443bc33fc8810c0a5d78f26c1bc00a13c01
VL <lusakvm@gmail.com>
2023-12-03 21:20:32 +0200

    PlaceImg created + refactoring

---
+++ client/src/components/PlaceImg.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const PlaceImg = ({ place, index = 0, className = null }) => {
+  if (!place.photos?.length) {
+    return "";
+  }
+  if (!className) {
+    className = "object-cover";
+  }
+  return (
+    <img
+      className={className}
+      src={"http://localhost:4000/uploads/" + place.photos[index]}
+      alt=""
+    />
+  );
+};
--- client/src/components/pages/PlacesPage.jsx
+++ client/src/components/pages/PlacesPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AccountNav } from "../AccountNav";
 import axios from "axios";
+import { PlaceImg } from "../PlaceImg";
 
 export const PlacesPage = () => {
   const [places, setPlaces] = useState([]);
@@ -43,13 +44,7 @@ export const PlacesPage = () => {
               className="flex cursor-pointer gap-4 bg-gray-200 p-4 rounded-2xl"
             >
               <div className="flex w-32 h-32 bg-gray-300 grow-1 shrink-0">
-                {place.photos.length > 0 && (
-                  <img
-                    className="object-cover"
-                    src={"http://localhost:4000/uploads/" + place.photos[0]}
-                    alt=""
-                  />
-                )}
+                <PlaceImg place={place} />
               </div>
               <div className="grow-2 shrink">
                 <h2 className="text-xl">{place.title}</h2>
